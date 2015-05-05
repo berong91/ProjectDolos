b32a29dc032fa73c292ded7a375d0b070dd9ce52
Phill Hood <oskatilla@gmail.com>
2015-05-05 14:54:04 -0700

    align test

--- www/src/Boot.js
+++ www/src/Boot.js
@@ -21,8 +21,8 @@ SpaceHipster.Boot.prototype = {
         this.scale.maxHeight = 1920;
 
         //have the game centered horizontally
-        this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
+        //this.scale.pageAlignHorizontally = true;
+        //this.scale.pageAlignVertically = true;
 
         //screen size will be set automatically
         // DEPRECATED this.scale.setScreenSize(true);
