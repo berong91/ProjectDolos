61373454e1c4ed5754719d2b8319148a2da98cae
Duy Pham <noob4ever8888@gmail.com>
2015-05-04 17:05:20 -0700

    Fix sript inport prob

--- www/src/main.js
+++ www/src/main.js
@@ -6,7 +6,8 @@ SpaceHipster.game.state.add('Boot', SpaceHipster.Boot);
 SpaceHipster.game.state.add('Preload', SpaceHipster.Preload);
 SpaceHipster.game.state.add('MainMenu', SpaceHipster.MainMenu);
 SpaceHipster.game.state.add('Game', SpaceHipster.Game);
-//paceHipster.game.state.add('Level', SpaceHipster.Level);
+
+//SpaceHipster.game.state.add('Level', SpaceHipster.Level);
 //SpaceHipster.game.state.add('Generator', SpaceHipster.Generator);
 
 SpaceHipster.game.state.start('Boot');
