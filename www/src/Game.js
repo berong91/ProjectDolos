c69e03974bc925cb3a4f7221e00178a4aedf4823
Tyler Trepanier <tmanthegamer@gmail.com>
2015-05-05 16:23:55 -0700

    Uncomment for all plane movements and interactions

--- www/src/Game.js
+++ www/src/Game.js
@@ -16,13 +16,26 @@ SpaceHipster.Game.prototype = {
         this.background = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
         this.generateAsteriods();
         
+		//sprites
+		//Test for moving a sprite left to right.
+		
+		/*
+		//plane is the object that is moving.
+		this.plane = this.game.add.sprite(this.game.world.centerX/10, this.game.world.centerY/1.5, 'playership');
+		this.plane.scale.setTo(5);
+		
+		//add physics to the sprites
+		this.game.physics.arcade.enable(this.plane);
+		this.plane.body.collideWorldBounds = true;
+		*/
+		
 		//sounds ADD THE DIFFERENT TYPE OF SOUNDS HERE
 		//refer to update function on how to use sounds
         this.explosionSound = this.game.add.audio('explosion');
     },
 
     generateAsteriods: function () {
-        this.asteroids = this.game.add.group();
+		this.asteroids = this.game.add.group();
         marker = this.add.graphics();
         marker.lineStyle(2, 0x000000, 1);
         marker.drawRect(0, 0, 160, 160);
@@ -33,7 +46,6 @@ SpaceHipster.Game.prototype = {
         //phaser's random number generator
         var numAsteroids = 9;
         var asteriod;
-
         for (var i = 0; i < numAsteroids; i++) {
             //add sprite
             asteriod = this.asteroids.create(xloc, yloc, 'pepe');
@@ -51,16 +63,24 @@ SpaceHipster.Game.prototype = {
             asteriod.body.velocity.y = 0;
             asteriod.body.immovable = true;
             asteriod.body.collideWorldBounds = true;
-            
+            /*
+			if (i === 5)
+				this.boom = asteriod;
+			*/
         }
     },
     update: function() {
     	//Sounds must be called inside of this function either directly or with another function entirely (except create and preload)
-		//this.playSound();
-    }
+		/*
+		this.plane.body.velocity.x = 25;
+		
+		---collision works, TURN DOWN YOUR SPEAKERS
+		this.game.physics.arcade.collide(this.plane, this.boom, this.playSound, null, this);
+		*/
+    },
 	
-	/*playSound: function() {
-		//this.explosionSound.play()
-	}*/
+	playSound: function() {
+		this.explosionSound.play();
+	},
 };
 
