096552a9777d22e4830ce6814bfde7612e0a9b1e
Tyler Trepanier <tmanthegamer@gmail.com>
2015-05-13 13:15:48 -0700

    Vehicles have smaller hitboxes

--- www/src/Game.js
+++ www/src/Game.js
@@ -11,6 +11,9 @@ var vehicles = [];
 var count;
 var text;
 
+//Score text
+var scoreText;
+
 //emitter for the explosion for vehicle collisions
 var emitter;
 
@@ -47,6 +50,7 @@ TMT.Game.prototype = {
 		//This will create text in the top left of the game screen.
 		text = this.game.add.text(50, 50, 'Time remaining: 60 seconds' , { fontSize: '32px', fill: '#FFF' });
 
+		
         //timer function-starts at 60, decrements one every 1000 ms
         count=60;
         var counter= setInterval(timer, 1000); 
@@ -89,6 +93,8 @@ TMT.Game.prototype = {
 		//add physics to the new plane.
         this.game.physics.arcade.enable(this.plane);
         this.plane.body.overlapWorldBounds = true;
+		this.plane.body.width = this.plane.body.width / 2;
+    	this.plane.body.height = this.plane.body.height / 2;
 	},
     /*
 		Create a boat. For now, only creates one boat as referenced by
@@ -107,6 +113,8 @@ TMT.Game.prototype = {
         //add physics to the new boat.
         this.game.physics.arcade.enable(this.boat);
         this.boat.body.overlapWorldBounds = true;
+		this.boat.body.width = this.boat.body.width / 2;
+    	this.boat.body.height = this.boat.body.height / 2;
     },
     /*
 		Create train. For now, only creates one train as referenced by
@@ -125,7 +133,8 @@ TMT.Game.prototype = {
         //Add physics to the new train.
         this.game.physics.arcade.enable(this.train);
         this.train.body.overlapWorldBounds = true;
-		
+		this.train.body.width = this.train.body.width / 2;
+    	this.train.body.height = this.train.body.height / 2;
     },
 
 	enableMoving: function(vehicle) {
@@ -186,12 +195,14 @@ TMT.Game.prototype = {
 		
 		//Sounds must be called inside of this function either directly
 		//or with another function entirely (except create and preload)
+		
+		//Checks whether or not the vehicle has been "allowed to move."
 		if(this.plane.moving)
 			this.plane.body.velocity.x = 80;
 		if(this.boat.moving)
-			this.boat.body.velocity.x = 60;
+			this.boat.body.velocity.x = 80;
 		if(this.train.moving)
-			this.train.body.velocity.x = 70;
+			this.train.body.velocity.x = 80;
 		
 		//Overlap that allows all members of vehicles to interact with 
 		//tiles.
@@ -275,9 +286,9 @@ TMT.Game.prototype = {
     */
 	onDown: function (tile, pointer) {
         if (!this.train.moving){
-			setTimeout(this.enableMoving(this.train), 3);
-			setTimeout(this.enableMoving(this.plane), 3);
-			setTimeout(this.enableMoving(this.boat), 3);
+			setTimeout(this.enableMoving(this.train), 300000000);
+			setTimeout(this.enableMoving(this.plane), 300000);
+			setTimeout(this.enableMoving(this.boat), 23400);
 		}
 		switchSound.play();
         if (tile.frame < 2)
