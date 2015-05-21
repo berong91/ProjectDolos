a3c5bfa1582d306586f548b2329bc143283a7a2c
Duy Pham <noob4ever8888@gmail.com>
2015-05-21 16:00:53 -0700

    WTF

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<?php
+    header("Location: game.html"); /* Redirect browser */
+    exit();
+    if (session_status() == PHP_SESSION_NONE || isset($_COOKIE['Username']) || strcmp($_COOKIE["Username"], "") == 0) {
+        session_start();
+        header("Location: login/"); /* Redirect browser */
+        exit();
+    } else {
+        header("Location: game.html"); /* Redirect browser */
+        exit();
+    }
+?>
